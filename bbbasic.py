9809a8b97db2df776092660f18c666594626564c
MD75545 <125954630+MD75545@users.noreply.github.com>
2023-02-21 12:39:48 +0530

    Add files via upload

---
+++ bbbasic.py
@@ -0,0 +1,14 @@
+eid,ename,esal = 111,"Mona",10000.45
+print(eid)
+print(ename)
+print(esal)
+print("Emp id =",eid)
+print("Emp name=",ename)
+print("esal=",esal)
+print(eid,ename,esal)
+print("Emp id=",eid,"Emp name=",ename,"Emp sal=",esal)
+
+print("Emp id =%d Emp name =%s Emp sal =%g" %(eid,ename,esal))
+print("Emp id{}Emp name:{}Emp sal{}".format(eid,ename,esal))
+print("Emp id{1}Emp name:{2}Emp sal{0}".format(eid,ename,esal))
+print("Emp id ={0}\nEmp name={1}\n Empsal ={2}".format(eid,ename,esal))
